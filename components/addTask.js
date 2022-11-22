aa7ee01139937e797f5c964bd7650357c7a25b16
Tessera04 <matute_11@hotmail.com.ar>
2022-11-21 22:11:05 -0300

    Ordenando archivo

--- components/addTask.js
+++ components/addTask.js
@@ -2,33 +2,36 @@ import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js';
 
 export const addTask = (evento) =>{
-    const list = document.querySelector('[data-list]');
-    const task = createTask(evento);
-    list.appendChild(task);
-  }
-  
-  const createTask = (evento) => {
     evento.preventDefault();
-    const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+    const list = document.querySelector('[data-list]');
     const input = document.querySelector('[data-form-input]');
     const calendar = document.querySelector('[data-form-date]');
+
     const value = input.value;
     const date = calendar.value;
     const dateFormat = moment(date).format('DD/MM/YYYY');
+     
+    input.value = '';
+    calendar.value = "";
+    const taskObj = {
+        value,
+        dateFormat
+    }
+
+    const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+    taskList.push(taskObj);
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+
+    const task = createTask(taskObj);
+    list.appendChild(task);
+  }
+  
+  const createTask = ({value, dateFormat}) => {
     const task = document.createElement('li');
     task.classList.add('card');
-    input.value = '';
-    //backticks
+
     const taskContent = document.createElement('div');
-  
-    const taskObj = {
-      value,
-      dateFormat
-    }
-  
-    taskList.push(taskObj)
-  
-    localStorage.setItem("tasks", JSON.stringify(taskList));
+
     //Session storage sirve para almacenar datos mientras la pagina este abierta
     //local storage sirve para almacenar datos, pero solo el ultimo que se ingrese
     const titleTask = document.createElement('span');
