25c4a3d72f611d27da44a05e2bfb1dad27a5739b
Tessera04 <matute_11@hotmail.com.ar>
2022-11-23 01:42:28 -0300

    Organizando por fecha

--- components/addTask.js
+++ components/addTask.js
@@ -1,5 +1,6 @@
 import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js';
+import { displayTask } from './readTask.js';
 
 export const addTask = (evento) =>{
     evento.preventDefault();
@@ -20,14 +21,15 @@ export const addTask = (evento) =>{
     const taskObj = {
         value,
         dateFormat
-    }
+    };
+
+    list.innerHTML = '';
 
     const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
     taskList.push(taskObj);
     localStorage.setItem("tasks", JSON.stringify(taskList));
 
-    const task = createTask(taskObj);
-    list.appendChild(task);
+    displayTask();
   }
   
   export const createTask = ({value, dateFormat}) => {
